0d58eeb2b157ec6fd0efd12bbb29f5bf4e76126f
Ana Carolina Alves Cruz <anac.alvescruz@gmail.com>
2020-06-12 17:11:48 -0300

    Nível 3 - Aula 2 completa

---
+++ gobarber-web/src/hooks/auth.tsx
@@ -0,0 +1,70 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarbet:token');
+    const user = localStorage.getItem('@GoBarbet:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarbet:token', token);
+    localStorage.setItem('@GoBarbet:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarbet:token');
+    localStorage.removeItem('@GoBarbet:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
---
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => <h1>dashboard</h1>;
+
+export default Dashboard;
